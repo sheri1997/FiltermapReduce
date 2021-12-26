290fd283891a333004a13a2b4f31a631d8e24675
Shreesh Pandey <shvspandey@gmail.com>
2021-12-26 22:04:49 +0530

    Usig Filter Method To Print The Even Numbers In The List

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-"""Displaying Welcome Message on The Master Branch
-    """
+'''Here We Are Using The Filter Method To find the
+    number of even numbers in the list inputted by the user'''
 
-print("Welcome To The Filter Map Reduce Problem")
+
+def filter_func(values):
+    data = filter(lambda x: x % 2 == 0, values)
+    return list(data)
+
+
+sequence = filter_func([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(sequence)
